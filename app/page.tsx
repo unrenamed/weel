064901a77eceb89e280a55c0c61ce7b4c3a31152
unrenamed <renkonazbka@gmail.com>
2023-11-07 17:10:12 +0200

    refactor: clean up root page file

--- app/page.tsx
+++ app/page.tsx
@@ -36,46 +36,6 @@ export default function Home() {
       >
         Create rand link
       </button>
-      <button
-        onClick={() =>
-          fetch("/api/links", {
-            method: "DELETE",
-            body: JSON.stringify({
-              domain: window.location.host,
-              key: "os",
-            }),
-            headers: {
-              "Content-Type": "application/json",
-            },
-          })
-        }
-      >
-        Delete link
-      </button>
-      <button
-        onClick={() =>
-          fetch("/api/links/os/stats/timeseries?interval=1h", {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-            },
-          })
-        }
-      >
-        Fetch analytics
-      </button>
-      <button
-        onClick={() =>
-          fetch("/api/links", {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-            },
-          })
-        }
-      >
-        Load products
-      </button>
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
