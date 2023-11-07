c1d1499d98b86e7fc37613cc10c6c434821e1bcc
unrenamed <renkonazbka@gmail.com>
2023-11-07 17:09:51 +0200

    feat: wrap app layout with Toaster component

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "@radix-ui/themes/styles.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Theme } from "@radix-ui/themes";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Toaster />
         <Theme>{children}</Theme>
       </body>
     </html>
