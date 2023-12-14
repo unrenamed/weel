8ed9ab07077586836f83c4c155b753155fde5c00
unrenamed <renkonazbka@gmail.com>
2023-12-14 11:58:41 +0200

    feat: add space between text and chevron in dropdown button

--- components/shared/button-dropdown.tsx
+++ components/shared/button-dropdown.tsx
@@ -13,7 +13,7 @@ export default function ButtonDropdown({ content, icon, text }: Props) {
   const [open, setOpen] = useState(false);
   return (
     <DropdownMenu content={content} onOpenChange={setOpen}>
-      <button className="flex items-center justify-between rounded-md shadow transition-all active:scale-95 px-3 py-2.5 bg-white hover:shadow-md w-full xs:w-48">
+      <button className="flex items-center justify-between space-x-2 rounded-md shadow transition-all active:scale-95 px-3 py-2.5 bg-white hover:shadow-md w-full xs:w-48">
         <div className="flex items-center space-x-2">
           {icon}
           <p className="text-sm">{text}</p>
