bce9ce6f44fa388355090b6a2d741a68f82be22a
unrenamed <renkonazbka@gmail.com>
2023-12-14 11:56:14 +0200

    feat: link to /links from /dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,17 @@
+import Link from "next/link";
+
 export default function DashboardPage() {
-  return <h1>Dashboard</h1>;
+  return (
+    <div className="w-full bg-gray-50">
+      <div className="flex place-content-center mt-10">
+        <Link href="/links" className="p-1">
+          <div className="group rounded-md px-3 py-2 transition-all duration-75 hover:bg-gray-200 active:bg-gray-300">
+            <p className="text-md text-gray-600 group-hover:text-black">
+              View my links →
+            </p>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 }
