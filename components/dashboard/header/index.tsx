140a994722e5d27e30d6f2f88ee8a0ef4cfb76c3
unrenamed <renkonazbka@gmail.com>
2023-12-15 21:16:34 +0200

    feat: redirect to home page on app logo click

--- components/dashboard/header/index.tsx
+++ components/dashboard/header/index.tsx
@@ -6,7 +6,7 @@ export default function AppHeader() {
     <div className="sticky left-0 right-0 top-0 z-40 border-b border-gray-200 bg-white flex place-content-center">
       <div className="w-full 2xl:w-3/5 xl:w-4/5 px-3">
         <div className="flex h-16 items-center justify-between">
-          <Link href="/links">
+          <Link href="/">
             <HeaderLogo />
           </Link>
         </div>
