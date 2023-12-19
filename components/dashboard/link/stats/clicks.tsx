86c571700aa9ff797ce8700fdba966023ae6d2cf
unrenamed <renkonazbka@gmail.com>
2023-12-19 18:48:42 +0200

    fix: show skeleton on timeseries data revalidation

--- components/dashboard/link/stats/clicks.tsx
+++ components/dashboard/link/stats/clicks.tsx
@@ -11,13 +11,23 @@ export default function Clicks() {
   const searchParams = useSearchParams();
   const interval = (searchParams.get("interval") as Interval) ?? "24h";
 
-  const { data: timeseries, isLoading: loadingTimeseries } = useSWR<
-    { t: string; clicks: number }[]
-  >(`/api/links/stats/timeseries?${searchParams}`, fetcher);
+  const {
+    data: timeseries,
+    isLoading: loadingTimeseries,
+    isValidating: validatingTimeseries,
+  } = useSWR<{ t: string; clicks: number }[]>(
+    `/api/links/stats/timeseries?${searchParams}`,
+    fetcher
+  );
 
-  const { data: clicksData, isLoading: loadingClicks } = useSWR<
-    { clicks: number }[]
-  >(`/api/links/stats/clicks?${searchParams}`, fetcher);
+  const {
+    data: clicksData,
+    isLoading: loadingClicks,
+    isValidating: validatingClicks,
+  } = useSWR<{ clicks: number }[]>(
+    `/api/links/stats/clicks?${searchParams}`,
+    fetcher
+  );
 
   const totalClicks = clicksData?.[0]?.clicks ?? 0;
 
@@ -26,7 +36,7 @@ export default function Clicks() {
       <div className="mb-5 flex items-start justify-between space-x-4">
         <div className="flex-none">
           <div className="flex items-end space-x-1">
-            {!loadingClicks && totalClicks ? (
+            {!loadingClicks && !validatingClicks && totalClicks ? (
               <NumberTooltip value={totalClicks} unit="total click">
                 <h1 className="text-3xl font-bold sm:text-4xl">
                   {nFormatter(totalClicks)}
@@ -42,7 +52,7 @@ export default function Clicks() {
           </p>
         </div>
       </div>
-      {loadingTimeseries ? (
+      {loadingTimeseries || validatingTimeseries ? (
         <div className="rounded animate-pulse bg-gray-200 w-full h-[300px] sm:h-[400px]" />
       ) : (
         <div className="h-[400px] w-full">
